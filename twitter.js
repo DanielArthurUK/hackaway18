3e0e6ddd0c23ca5c32e1339b92ef7240b82f751b
Harry Collins <harry@Harrys-MacBook-Pro.local>
2018-01-13 20:06:44 +0000

    twitter stuff

---
+++ twitter.js
@@ -0,0 +1,25 @@
+
+
+import twitter, {auth} from 'react-native-twitter';
+
+export default postToTwitter = () => {
+  auth({
+          consumerKey:       'yXXNSwhnphe4uXwxz7EDR50bq',
+          consumerSecret:    'xZkQcqWEUFBmzIkz5uESb6AK6Ix6ZzDLozU36jvEK6OIaRbn0F',
+        //  accessToken:       '716332538-Yxh0TeMJnEbhbxfuaBBoQheZh29FOT1D21aQpe6q',
+        //  accessTokenSecret: 'wObTovEnts2w4jWDRrtB9y7mfNWPqAgzqAOYKqUpH5Ea8',
+      })
+  const tokens = {
+    consumerKey:       'yXXNSwhnphe4uXwxz7EDR50bq',
+    consumerSecret:    'xZkQcqWEUFBmzIkz5uESb6AK6Ix6ZzDLozU36jvEK6OIaRbn0F',
+    accessToken:       '716332538-Yxh0TeMJnEbhbxfuaBBoQheZh29FOT1D21aQpe6q',
+    accessTokenSecret: 'wObTovEnts2w4jWDRrtB9y7mfNWPqAgzqAOYKqUpH5Ea8',
+  }
+
+  const {rest, stream} = twitter(tokens);
+
+  const message = "Hello World";
+
+  rest.post('post/statuses/update', {message});
+
+}
