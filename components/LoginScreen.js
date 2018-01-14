32f3ac29fc48835b14bd088d2df7b22d5a6b7438
Dan <danny_arthur@hotmail.co.uk>
2018-01-14 02:49:12 +0000

    UI fixes

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text, Button} from 'react-native-elements';
+// import postToTwitter from './utilities/twitter';
 
 class LoginScreen extends Component {
+
+    handleTwitterLogin = () => {
+        // postToTwitter();
+        this.props.navigation.navigate('AuthorizeScreen');
+    }
+
     render() {
         return (
             <View style={styles.flexCenter}>
                 <Text h4>Welcome to Tweetophile</Text>
-                <Button icon={{name: 'twitter', type: 'font-awesome'}} rounded backgroundColor={twitterBlue} title="Log in with Twitter" />
+                <Button
+                    icon={{name: 'twitter', type: 'font-awesome'}}
+                    rounded
+                    backgroundColor={twitterBlue}
+                    title="Log in with Twitter"
+                    onPress={this.handleTwitterLogin}/>
             </View>
         );
     }
